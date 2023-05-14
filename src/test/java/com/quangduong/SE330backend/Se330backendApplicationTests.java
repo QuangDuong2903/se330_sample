fc41882e719fc1c6c92d5d677f1dc3bc345dfc71
Quang Duong <quangduongptsc@gmail.com>
2023-05-14 22:31:25 +0700

    first commit

---
+++ src/test/java/com/quangduong/SE330backend/Se330backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangduong.SE330backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se330backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
