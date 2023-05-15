bb98f4cae445485c376aa0d5bd89dcc5f7323c08
Quang Duong <quangduongptsc@gmail.com>
2023-05-15 18:04:37 +0700

    complete chat

--- src/main/java/com/quangduong/SE330backend/service/TaskService.java
+++ src/main/java/com/quangduong/SE330backend/service/TaskService.java
@@ -1,12 +1,13 @@
 package com.quangduong.SE330backend.service;
 
 import com.quangduong.SE330backend.dto.task.TaskDTO;
+import com.quangduong.SE330backend.dto.task.TaskDetailsDTO;
 import com.quangduong.SE330backend.dto.task.TaskUpdateDTO;
 
 public interface TaskService {
 
-    TaskDTO createTask(TaskDTO dto);
-    TaskDTO updateTask(TaskUpdateDTO dto);
+    TaskDetailsDTO createTask(TaskDTO dto);
+    TaskDetailsDTO updateTask(TaskUpdateDTO dto);
     void deleteTask(long id);
 
 }
